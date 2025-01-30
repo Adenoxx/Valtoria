51e65df8812da1df88910fcdb5bfa5c88f86bdcb
Noble Unyimeabasi Udoesit <noblenoble2014@gmail.com>
2025-01-30 14:48:02 +0100

    first commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,20 @@
-import "@/styles/globals.css";
+import localFont from 'next/font/local';
+import 'rsuite/dist/rsuite-no-reset.min.css';
+import { CustomProvider } from 'rsuite';
+
+
+const satoshi = localFont({
+  src: './fonts/Satoshi-Variable.woff2',
+  display: 'swap'
+})
+
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+  <CustomProvider>
+    <main className={satoshi.className}>
+  <Component {...pageProps} />
+  </main>
+  </CustomProvider>  
+  );
 }
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
+// import satoshi from '../styles/fonts';
 
 export default function Document() {
   return (
